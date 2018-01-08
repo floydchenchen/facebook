2df08c78fee0e3abccc39958234f2c177d673908
foydchenchen <floydchenchen@gmail.com>
2018-01-08 15:35:27 -0600

    01/08/18 15:35

--- tag/valid_palindrome.java
+++ tag/valid_palindrome.java
@@ -33,4 +33,33 @@ public class valid_palindrome {
         }
         return true;
     }
+
+    /*
+    *
+    * valid palindrome II
+    *
+    * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+    *
+    * */
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            i++; j--;
+        }
+
+        if (i >= j) return true;
+
+        if (isPalin(s, i + 1, j) || isPalin(s, i, j - 1)) return true;
+        return false;
+    }
+
+    private boolean isPalin(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++; j--;
+            }
+            else return false;
+        }
+        return true;
+    }
 }
